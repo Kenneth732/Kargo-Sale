8c0a6115d8be5305e4d155951e5a296def183b41
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-05-29 11:12:29 +0300

    ux

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,7 +13,7 @@ function Home() {
 
             <section className="about-section section-padding" id="section_2">
                 <div className="container">
-                    <div class="row align-items-center">
+                    <div className="row align-items-center">
 
                         <div className="col-lg-5 col-12">
                             <small className="section-small-title">Our Story</small>
