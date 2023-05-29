ebfb00fd352af987a1a5d34c4c23f47a124c5467
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-05-29 10:32:04 +0300

    ux

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function About() {
 <section id="about" classNane="about">
       <div className="container" data-aos="fade-up">
 
-        <div class="section-title">
+        <div className="section-title">
           <h2>About</h2>
           <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
         </div>
